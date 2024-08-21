8237af03526a295438aa958e25bbebfbd200dd35
Jay Engineer <jayengineer@Jays-MacBook-Pro-2.local>
2024-08-21 14:41:35 -0400

    updated por

--- index.py
+++ index.py
@@ -50,5 +50,5 @@ def get_subtitles():
     return response
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
